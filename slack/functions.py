96465d0d434a136f72424437981ee943507ba457
Iokin Pardo <iokin.pardo@bts.com>
2023-09-29 17:57:20 +0200

    original

--- slack/functions.py
+++ slack/functions.py
@@ -9,24 +9,17 @@ from langchain.prompts.chat import (
 
 load_dotenv(find_dotenv())
 
-
 def draft_email(user_input, name="Dave"):
     chat = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=1)
 
     template = """
-    
     You are a helpful assistant that drafts an email reply based on an a new email.
-    
     Your goal is to help the user quickly create a perfect email reply.
-    
     Keep your reply short and to the point and mimic the style of the email so you reply in a similar manner to match the tone.
-    
     Start your reply by saying: "Hi {name}, here's a draft for your reply:". And then proceed with the reply on a new line.
-    
     Make sure to sign of with {signature}.
-    
     """
-
+    
     signature = f"Kind regards, \n\{name}"
     system_message_prompt = SystemMessagePromptTemplate.from_template(template)
 
@@ -40,6 +33,7 @@ def draft_email(user_input, name="Dave"):
     chain = LLMChain(llm=chat, prompt=chat_prompt)
     response = chain.run(user_input=user_input, signature=signature, name=name)
     
+    """
     API_URL = "https://ai.btsmomenta.com/api/v1/prediction/80f44dc0-fc05-40dc-9ae3-974f38054eaa"
 
     def query(payload):
@@ -48,12 +42,13 @@ def draft_email(user_input, name="Dave"):
         return response.json()
         
     response = query({"question": user_input})
-
+    """
+    
     return response
 
 
 def call_flowise(message):
-     """
+    """
     API_URL = "https://ai.btsmomenta.com/api/v1/prediction/80f44dc0-fc05-40dc-9ae3-974f38054eaa"
 
     def query(payload):
@@ -63,4 +58,4 @@ def call_flowise(message):
     return response = query({
         "question": message,
     })
-     """
+    """
